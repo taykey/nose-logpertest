10b6df3a853454373252a2d926d4a38e195b0470
Roy <roy@taykey.com>
2014-11-25 17:22:53 +0200

    updated the setup module

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 README = open(os.path.join(ROOT, 'README.rst')).read()
+CHANGES = open(os.path.join(ROOT, 'CHANGES.md')).read()
 
 requires = [
     'nose',
@@ -13,23 +14,23 @@ requires = [
 
 setup(name='nose-logpertest',
       version='0.0.1',
-      description='logging plugin to create log per test',
-      long_description=README,
+      description='Logging plugin to create log per test',
+      long_description=README + '\n\n' + CHANGES,
       classifiers=[
+          "Programming Language :: Python",
           "Development Status :: 4 - Beta",
           "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
+          "License :: OSI Approved :: Apache Software License",
           "Operating System :: OS Independent",
-          "Programming Language :: Python",
           "Topic :: Software Development :: Testing",
       ],
-      license='MIT',
+      license='Apache Software License',
       author='Roy Klinger',
       author_email='roy@taykey.com',
-      url='',
+      url='https://github.com/taykey/nose-logpertest',
       packages=find_packages(),
       keywords='nosetest logging',
-      include_package_data=True,
+      include_package_data=False,
       zip_safe=False,
       entry_points="""\
       [nose.plugins.0.10]
